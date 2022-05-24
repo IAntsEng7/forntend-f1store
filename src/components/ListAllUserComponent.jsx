ca588f8be7df13f4f581765bb23038185a41e05e
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-24 16:22:02 +0800

    List All

---
+++ src/components/ListAllUserComponent.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import UserService from "../services/UserService";
+
+export default class ListAllUserComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    UserService.getUsers().then((response) => {
+      this.setState({ users: response.data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">User List</h2>
+        <div className="row">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>UID</th>
+                <th>User Account</th>
+                <th>User Name</th>
+                <th>User Gender</th>
+                <th>User Phone</th>
+                <th>User Email Address</th>
+                <th>Create Date</th>
+                <th>Last modified Date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map((user) => (
+                <tr key={user.uId}>
+                  <td>{user.uAccount}</td>
+                  <td>{user.uName}</td>
+                  <td>{user.uGender}</td>
+                  <td>{user.uPhone}</td>
+                  <td>{user.uEmail}</td>
+                  <td>{user.createTime}</td>
+                  <td>{user.modifiedTime}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
